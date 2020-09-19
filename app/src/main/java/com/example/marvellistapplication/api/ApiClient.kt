4de81f98f1906356033a38d2c8e8dba40d1846cf
Helder Rocha <hedie.strokes@gmail.com>
2020-09-19 15:34:55 -0300

    API Configs

--- app/src/main/java/com/example/marvellistapplication/api/ApiClient.kt
+++ app/src/main/java/com/example/marvellistapplication/api/ApiClient.kt
@@ -1,8 +1,16 @@
 package com.example.marvellistapplication.api
 
+import com.example.marvellistapplication.api.model.Response
+import io.reactivex.Observable
 import javax.inject.Inject
 
 
 class ApiClient @Inject constructor(private val marvelApi: MarvelApiApi) {
-   //TODO
+    fun characters(apiKey: String, hashApi: String, offset: Long): Observable<Response> {
+        return marvelApi.characters(apiKey, hashApi, offset)
+    }
+
+    fun charactersId(id: Long, apiKey: String, hashApi: String): Observable<Response> {
+        return marvelApi.charactersId(id, apiKey, hashApi)
+    }
 }
--- app/src/main/java/com/example/marvellistapplication/api/MarvelApiApi.kt
+++ app/src/main/java/com/example/marvellistapplication/api/MarvelApiApi.kt
@@ -1,5 +1,6 @@
 package com.example.marvellistapplication.api
 
+import com.example.marvellistapplication.api.model.Response
 import java.util.*
 import io.reactivex.Observable
 import retrofit2.http.GET
@@ -9,6 +10,7 @@ import java.util.*
 
 
 interface MarvelApiApi {
+
     companion object {
         val ts = (Calendar.getInstance(TimeZone.getTimeZone("UTC")).timeInMillis / 1000L).toString()
         const val URL = "https://gateway.marvel.com/v1/public/"
@@ -16,9 +18,21 @@ interface MarvelApiApi {
         const val API_HASH = "dba4e3a0288d0a5fdddf64aa6bae3d80"
         const val LIMIT = "2"
         const val OFFSET = "0"
-        // val TS = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
         val TS = "1"
     }
 
-   //TODO
+    @GET("characters?limit=10&ts=1")
+    fun characters(
+        @Query("apikey") apiKey: String,
+        @Query("hash") hashApi: String,
+        @Query("offset") offset: Long
+    ): Observable<Response>
+
+    @GET("characters/{id}?limit=7&ts=1")
+    fun charactersId(
+        @Path("id") id: Long,
+        @Query("apikey") apiKey: String,
+        @Query("hash") hashApi: String
+    ): Observable<Response>
+
 }
